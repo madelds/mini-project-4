2746edf564e9ca9a90cefadec6cbd70229039333
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2023-09-28 18:00:22 -0500

    Ready for prime time (I hope).

--- src/AssociativeArrayTests.java
+++ src/AssociativeArrayTests.java
@@ -98,18 +98,68 @@ public class AssociativeArrayTests {
    */
   @Test
   public void samuelRebelskyTest01() {
+    // Build an array
     AssociativeArray<String, String> arr = new AssociativeArray<String, String>();
     arr.set("A", "Apple");
     try {
       assertEquals("Apple", arr.get("A"));
     } catch (Exception e) {
-      fail("Exception in call to get()");
+      fail("Original array does not contain expected value");
+    }
+    // Make a copy
+    AssociativeArray<String, String> arr2 = arr.clone();
+    // Make sure it contains the appropriate value
+    try {
+      assertEquals("Apple", arr2.get("A"));
+    } catch (Exception e) {
+      fail("Clone does not contain original value");
+    } // try/catch
+    // Change the original array
+    arr.set("A", "aardvark");
+    // Make sure we haven't changed the clone.
+    try {
+      assertEquals("Apple", arr2.get("A"));
+    } catch (Exception e) {
+      fail("Change to original changes clone.");
+    }
+    // Change the clone
+    arr2.set("A", "Ant");
+    // And look for values
+    try {
+      assertEquals("Ant", arr2.get("A"));
+    } catch (Exception e) {
+      fail("Cannot change clone");
+    }
+    try {
+      assertEquals("aardvark", arr.get("A"));
+    } catch (Exception e) {
+      fail("Change to clone changes original");
     }
   } // samuelRebelskyTest01()
 
   /**
+   * Can we successfully add a bunch of values?  (Checks array expansion.)
+   */
+  @Test
+  public void samuelRebelskyTest02() {
+    AssociativeArray<Integer,Integer> arr = new AssociativeArray<Integer,Integer>();
+    // Add a bunch of values
+    for (int i = 10; i < 50; i++) {
+      arr.set(i, i*i);
+    } // for
+    try {
+      for (int i = 49; i >= 10; i--) {
+        assertEquals(i*i, arr.get(i));
+      }
+    } catch (Exception e) {
+      fail("Exception in call to get");
+    }
+  } // samuelRebelskyTest02()
+
+  /**
    * Do we get exceptions when grabbing a deleted value from the array?
    */
+  @Test
   public void samuelRebelskyTest03() {
     AssociativeArray<String, String> arr = new AssociativeArray<String, String>();
     // Add an element to the array
